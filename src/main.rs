02ed27c8258d48228394935e2a6167372fee3bc4
Jasper Niemeyer <jani5714@colorado.edu>
2026-01-11 20:43:43 -0700

    Collapse nested if-let in logger init

--- src/main.rs
+++ src/main.rs
@@ -15,14 +15,13 @@ fn main() -> color_eyre::Result<()> {
     color_eyre::install()?;
 
     // Initialize file logging (ignore errors - logging is optional)
-    if let Ok(log_path) = config::get_log_path() {
-        if let Ok(file) = std::fs::OpenOptions::new()
+    if let Ok(log_path) = config::get_log_path()
+        && let Ok(file) = std::fs::OpenOptions::new()
             .create(true)
             .append(true)
             .open(&log_path)
-        {
-            let _ = WriteLogger::init(LevelFilter::Info, LogConfig::default(), file);
-        }
+    {
+        let _ = WriteLogger::init(LevelFilter::Info, LogConfig::default(), file);
     }
 
     let terminal = ratatui::init();
